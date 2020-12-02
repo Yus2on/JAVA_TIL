01c7e3b4b62f78aa2f20ca5388d8f531190dfb39
yus2on <gbwl2784@gmail.com>
2020-12-02 22:42:26 +0900

    mv JAVA/ch04

--- ch04/src/p02/ArithmathicOperator.java
+++
@@ -1,9 +0,0 @@
-package p02;
-
-// 다형성의 힘 ...
-// 동일한 메소드로 인터페이싱.
-public interface ArithmathicOperator {
-    int operate(int x, int y);
-    long operate(long x, long y);
-    double operate(double x, double y);
-}
