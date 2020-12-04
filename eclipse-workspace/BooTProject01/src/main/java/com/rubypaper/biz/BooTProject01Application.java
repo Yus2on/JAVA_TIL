d3547dc0485cbbe51b19273b6b27e63647d30891
yus2on <gbwl2784@gmail.com>
2020-12-04 17:06:14 +0900

    ADD: BooT Project

---
+++ eclipse-workspace/BooTProject01/src/main/java/com/rubypaper/biz/BooTProject01Application.java
@@ -0,0 +1,13 @@
+package com.rubypaper.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooTProject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooTProject01Application.class, args);
+	}
+
+}
